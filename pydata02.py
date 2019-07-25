9c057dad9cd897486c04984359fac1f319c62e9b
ZX <hsieh01@outlook.com>
2019-07-25 08:50:49 -0500

    Update pydata02.py

--- pydata02.py
+++ pydata02.py
@@ -1,3 +1,15 @@
 ################################################
 #python for data analysis study codes and notes#
 ################################################
+
+
+
+#2.1
+#%%
+print('Hello world')
+
+#2.2 
+#%%
+import numpy as np
+data = {i : np.random.randn() for i in range(7)}
+data
