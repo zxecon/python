7bd7ab8056d18b0392a1df2a9e05c8f517f15b0c
ZX <hsieh01@outlook.com>
2019-07-25 09:02:22 -0500

    Update pydata02.py

--- pydata02.py
+++ pydata02.py
@@ -6,15 +6,35 @@ Python for Data Analysis: Data Wrangling with Pandas, NumPy, and IPython 2nd Edi
 Visual Studio Code w/ Jupyter Environment
 '''
 
-#2.1
-#%%
-print('Hello world')
-
 #2.2 
+
 #%%
-pip install numpy #if numpy is not installed
+pip install numpy
+
+#%%
 import numpy as np
 data = {i : np.random.randn() for i in range(7)}
 data
+
+#%%
+b = [1, 2, 3]
+b?
+
+#%%
+print?
+
+#%run command
+#ipython_script_test.py
+'''
+def f(x, y, z):
+    return (x + y) / z
+
+a = 5
+b = 6
+c = 7.5
+
+result = f(a, b, c)
+'''
+#%run ipython_script_test.py
 
 
